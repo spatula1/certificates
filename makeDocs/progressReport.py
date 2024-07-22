69d841fdd70575594479f87722e821c1a171999d
spatula1 <skylaru07@gmail.com>
2024-07-22 11:28:32 -0700

    Update progressReport.py

--- makeDocs/progressReport.py
+++ makeDocs/progressReport.py
@@ -10,6 +10,21 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from makeDocs import parseRoster
 import makeDocs.parseLaneChart as parseLaneChart
 
+'''
+page 0 - lions and cubs, ss1, ss2
+page 1 - ss3, ss4
+page 2 - basic 5, 6
+page 3 - basic 1, 2, 3, 4
+page 4 - prefree, free 1, 2
+page 5 - freeskate 3, 4, 5, 6
+page 6 - adult gold
+page 7 - adult bronze, silver
+page 8 - LK SS, Adv SS
+page 9 - academy 1, 2
+page 10 - spin comp 1 2 // intro to spin
+page 11 - power
+'''
+
 def generate_progress_reports(lane_chart_stream, roster_stream, date):
     # Load the roster and lane chart data from in-memory streams
     fullClasses = parseRoster.load_progress_roster(io.BytesIO(roster_stream.read()))
@@ -43,7 +58,6 @@ def generate_progress_reports(lane_chart_stream, roster_stream, date):
     pageToInsert = 0
     side = 1  # which side to print on
     index = 0
-    laneChartIndex = 0
 
     for aClass in fullClasses:
         className = aClass[0]
@@ -65,15 +79,43 @@ def generate_progress_reports(lane_chart_stream, roster_stream, date):
 
         del aClass[1:3]  # remove class time and day
 
+        #change page based on class
         if className in ["Lions & Cubs", "Snowplow Sam 1", "Snowplow Sam 2"]:
             pageToInsert = 0
             x1, x2, x3, x5, x7, y1, y2, y3, y4, y5, y6, y7, y8 = 85, 60, 300, 480, 690, 134, 150, 150, 555, 137, 153, 152.5, 558
         elif className in ["Snowplow Sam 3", "Snowplow Sam 4"]:
             pageToInsert = 1
             x1, x2, x3, x5, x7, y1, y2, y3, y4, y5, y6, y7, y8 = 100, 60, 305, 485, 700, 134, 150, 150, 555, 133, 150, 150, 555
-        # (Other class-to-page mappings are similar to above)
-        # Add the rest of your class-to-page mappings here...
-
+        elif className in ["Basic 5", "Basic 6"]:
+            pageToInsert = 2
+            x1, x2, x3, x5, x7, y1, y2, y3, y4, y5, y6, y7, y8 = 100, 60, 300, 490, 700, 138, 154, 154, 560, 139, 154, 154, 560
+        elif className in ["Basic 4", "Basic 3", "Basic 2", "Basic 1"]:
+            pageToInsert = 3
+            x1, x2, x3, x5, x7, y1, y2, y3, y4, y5, y6, y7, y8 = 100, 60, 300, 490, 700, 136, 152, 152, 570, 137, 152, 152, 570
+        elif className in ["Pre-Freeskate", "Freeskate 1", "Freeskate 2"]:
+            pageToInsert = 4
+            x1, x2, x3, x5, x7, y1, y2, y3, y4, y5, y6, y7, y8 = 95, 60, 300, 490, 700, 134, 151, 151, 571, 134, 151, 151, 571
+        elif className in ["Freeskate 3", "Freeskate 4", "Freeskate 5", "Freeskate 6"]:
+            pageToInsert = 5
+            x1, x2, x3, x5, x7, y1, y2, y3, y4, y5, y6, y7, y8 = 100, 60, 315, 490, 700, 131, 146, 146, 573, 134, 149, 149, 575
+        elif className in ["Adult Gold"]:
+            pageToInsert = 6
+            x1, x2, x3, x5, x7, y1, y2, y3, y4, y5, y6, y7, y8 = 90, 60, 300, 480, 700, 135, 151, 151, 562, 138, 154, 154, 560
+        elif className in ["Adult Bronze", "Adult Silver"]:
+            pageToInsert = 7
+            x1, x2, x3, x5, x7, y1, y2, y3, y4, y5, y6, y7, y8 = 90, 60, 300, 480, 700, 135, 151, 151, 562, 136, 151, 151, 560
+        elif className in ["LK Skating Skills 8U", "LK Skating Skills 14U", "LK Adv Skating Skills 8U", "LK Adv Skating Skills 14U"]:
+            pageToInsert = 8
+            x1, x2, x3, x5, x7, y1, y2, y3, y4, y5, y6, y7, y8 = 90, 50, 300, 485, 700, 146, 170, 170, 562, 146, 170, 170, 560
+        elif className in ["LK Academy 1 8U", "LK Academy 1 14U", "LK Academy 2 8U", "LK Academy 2 14U"]:
+            pageToInsert = 9
+            x1, x2, x3, x5, x7, y1, y2, y3, y4, y5, y6, y7, y8 = 90, 50, 300, 485, 700, 146, 170, 170, 562, 146, 170, 170, 560
+        elif className in ["Spin Comp 1", "Spin Comp 2", "Intro To Spin"]:
+            pageToInsert = 10
+            x1, x2, x3, x5, x7, y1, y2, y3, y4, y5, y6, y7, y8 = 90, 50, 300, 485, 700, 146, 170, 170, 562, 146, 170, 170, 560
+        elif className in ["Power 1", "Power 2"]:
+            pageToInsert = 11
+            x1, x2, x3, x5, x7, y1, y2, y3, y4, y5, y6, y7, y8 = 90, 50, 300, 485, 700, 146, 170, 170, 562, 146, 170, 170, 560
         if index == 0:
             previousPageToInsert = pageToInsert
             index = 1
