e8576766f192a70a9e6f242aca96a552f68e58d2
spatula1 <skylaru07@gmail.com>
2024-10-21 18:19:44 -0700

    Update badges.py

--- makeDocs/badges.py
+++ makeDocs/badges.py
@@ -42,7 +42,6 @@ def generate_presigned_url(bucket_name, file_name, expiration=3600):
     
 # Generate mini class badges
 def generate_mini_badges(roster_stream, badge_template_stream):
-    
     # Prepare to create the output PDF
     output_pdf_stream = io.BytesIO()
 
@@ -51,6 +50,9 @@ def generate_mini_badges(roster_stream, badge_template_stream):
     badgeNum = 1
     pageNum = 0
     
+    # Reset the stream position of the badge_template_stream
+    badge_template_stream.seek(0)
+
     # Convert BytesIO stream to bytes
     badge_template_bytes = badge_template_stream.getvalue()
 
@@ -59,73 +61,36 @@ def generate_mini_badges(roster_stream, badge_template_stream):
     page = output_pdf.load_page(0)
     rotation = page.rotation  # Turns the text to face the right way
 
-    '''badge order
-    1 2
-    3 4
-    5 6
-    '''
-
     # Function to print info on badges
     def printInfo(y1: int, y2: int, x1: int, x2: int, x3: int, string: str, wordLen: int, size: int):
-        if badgeNum in [1, 3, 5]:
-            y = y1
-        else:
-            y = y2
-        if badgeNum in [1, 2]:
-            x = x1
-        elif badgeNum in [3, 4]:
-            x = x2
-        else:
-            x = x3
-        # Change font if name is too long 
-        if len(string) > wordLen:
-            fontSize = size
-        else:
-            fontSize = 25
-
-        if string == "Thurs":
-            fontSize = 14
-        # Print information on badge
-        page.insert_text((x, y), string, fontSize, fontname="helv", rotate=rotation, overlay=True)
+        # (existing code for printInfo)
+        ...
 
     # Prints names on badges
     for aClass in miniClasses:
-        className = aClass[0]
-        day = aClass[1]
-        time = aClass[2]
-        for x in range(3):
-            aClass.pop(0)
-        for word in aClass:
-            # Name
-            printInfo(560, 260, 177, 440, 706, word, 15, 20)
+        # (existing code for printing names, classes, days, etc.)
+        ...
 
-            # Class
-            printInfo(570, 262, 217, 480, 743, className, 9, 15)
+        # Change badge number and add new badge pages when needed
+        if badgeNum == 6:
+            badgeNum = 1  
+            pageNum += 2
+            
+            # Reset the stream position of the badge_template_stream again before re-opening
+            badge_template_stream.seek(0)  
+            tempDoc = fitz.open(badge_template_stream)  # Re-open the template stream
+            output_pdf.insert_pdf(tempDoc)
+            page = output_pdf.load_page(pageNum)
+        else:
+            badgeNum += 1
 
-            # Day
-            printInfo(430, 125, 217, 480, 743, day, 0, 20)
-
-            # Time
-            printInfo(380, 74, 217, 480, 743, time, 0, 15)
-
-            # Change badge num
-            if badgeNum == 6:
-                badgeNum = 1  
-                pageNum += 2 
-                # Add new badge pages
-                tempDoc = fitz.open(badge_template_stream)
-                output_pdf.insert_pdf(tempDoc)
-                page = output_pdf.load_page(pageNum)   
-            else:
-                badgeNum += 1
-    
     # Save the output PDF to the stream
     output_pdf.save(output_pdf_stream)
     output_pdf_stream.seek(0)  # Reset the stream position to the beginning
 
     return output_pdf_stream
 
-#generate full class badges
+
 def generate_full_badges(roster_stream, badge_template_stream):
     # Prepare to create the output PDF
     output_pdf_stream = io.BytesIO()
@@ -135,6 +100,9 @@ def generate_full_badges(roster_stream, badge_template_stream):
     badgeNum = 1
     pageNum = 0
 
+    # Reset the stream position of the badge_template_stream
+    badge_template_stream.seek(0)
+
     # Convert BytesIO stream to bytes
     badge_template_bytes = badge_template_stream.getvalue()
 
@@ -143,70 +111,28 @@ def generate_full_badges(roster_stream, badge_template_stream):
     page = output_pdf.load_page(0)
     rotation = page.rotation  # Turns the text to face the right way
 
-    '''badge order
-    1 2
-    3 4
-    5 6
-    '''
-
     # Function to print info on badges
     def printInfo(y1: int, y2: int, x1: int, x2: int, x3: int, string: str, wordLen: int, size: int):
-        if badgeNum in [1, 3, 5]:
-            y = y1
-        else:
-            y = y2
-        if badgeNum in [1, 2]:
-            x = x1
-        elif badgeNum in [3, 4]:
-            x = x2
-        else:
-            x = x3
-        # Change font if name is too long 
-        if len(string) > wordLen:
-            fontSize = size
-        else:
-            fontSize = 25
-        #change string name to fit on line 
-        if string == "LK Adv Skating Skills 8U":
-            string = "LK Adv SS 8U"
-        if string == "LK Adv Skating Skills 14U":
-            string = "LK Adv SS 14U"
-        
-        if string == "Thurs":
-            fontSize = 14
-        # Print information on badge
-        page.insert_text((x, y), string, fontSize, fontname="helv", rotate=rotation, overlay=True)
+        # (existing code for printInfo)
+        ...
 
     # Prints names on badges
     for aClass in fullClasses:
-        className = aClass[0]
-        day = aClass[1]
-        time = aClass[2]
-        for x in range(3):
-            aClass.pop(0)
-        for word in aClass:
-            # Name
-            printInfo(560, 260, 177, 440, 706, word, 15, 20)
+        # (existing code for printing names, classes, days, etc.)
+        ...
 
-            # Class
-            printInfo(570, 262, 217, 480, 743, className, 9, 15)
-
-            # Day
-            printInfo(430, 125, 217, 480, 743, day, 0, 20)
-
-            # Time
-            printInfo(380, 74, 217, 480, 743, time, 0, 15)
-
-            # Change badge num
-            if badgeNum == 6:
-                badgeNum = 1  
-                pageNum += 2 
-                # Add new badge pages
-                tempDoc = fitz.open(stream=badge_template_bytes, filetype="pdf")
-                output_pdf.insert_pdf(tempDoc)
-                page = output_pdf.load_page(pageNum)
-            else:
-                badgeNum += 1
+        # Change badge number and add new badge pages when needed
+        if badgeNum == 6:
+            badgeNum = 1  
+            pageNum += 2
+            
+            # Reset the stream position of the badge_template_stream again before re-opening
+            badge_template_stream.seek(0)  
+            tempDoc = fitz.open(stream=badge_template_bytes, filetype="pdf")  # Re-open the template stream
+            output_pdf.insert_pdf(tempDoc)
+            page = output_pdf.load_page(pageNum)
+        else:
+            badgeNum += 1
 
     # Save the output PDF to the stream
     output_pdf.save(output_pdf_stream)
